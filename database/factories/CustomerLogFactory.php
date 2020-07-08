10ed023427e63d73efb553e79b1fb494b4bf28d6
ttanaka9211 <ttanaka@hatchdogs.net>
2020-07-08 22:23:57 +0900

    firstcommit

---
+++ database/factories/CustomerLogFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\CustomerLog;
+use Faker\Generator as Faker;
+
+$factory->define(CustomerLog::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
