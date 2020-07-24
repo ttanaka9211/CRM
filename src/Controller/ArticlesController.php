2fd95d507ad9ab70cbf21ead712b085df98169a2
ttanaka9211 <ttanaka@hatchdogs.net>
2020-07-24 21:10:21 +0900

    contoller

--- src/Controller/ArticlesController.php
+++ src/Controller/ArticlesController.php
@@ -73,4 +73,16 @@ class ArticlesController extends AppController
             return $this->redirect(['action' => 'index']);
         }
     }
+
+    public function tags()
+    {
+        $tags = $this->request->getParam('pass');
+        $articles = $this->Articles->find('tagged', [
+            'tags' => $tags
+        ]);
+        $this->set([
+            'articles' => $articles,
+            'tags' => $tags
+        ]);
+    }
 }
